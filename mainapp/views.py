72d26c38aff262ab246472516cd90ff3c46ec8b8
Aswin Sankar P D <you@example.com>
2021-12-31 15:26:27 +0530

    rendering

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 def dashboard(request):
-    return render(request,'mainapp/dashboard.html')
+    order = Order.objects.all()
+    customer = Customer.objects.all()
+    total_orders = order.count()
+    customer_count = customer.count()
+    delivered_count = Order.objects.all().filter(status="Delivered").count()
+
+    context = {"order":order,"customer":customer,"total_orders":total_orders,"customer_count":customer_count,"delivered_count":delivered_count}
+    return render(request,'mainapp/dashboard.html',context)
 
 def products(request):
-    return render(request,'mainapp/product.html')
+    products = Product.objects.all()
+    context = {"product":products}
+    print(products)
+    return render(request,'mainapp/product.html',context)
 
 def customer(request):
     return render(request,'mainapp/customer.html')
